e753bc6fcb162e4c65d0adb7d085aba7c9b22991
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-03-04 20:44:16 +0000

    run tests

--- test/terraform_basic_example_test.go
+++ test/terraform_basic_example_test.go
@@ -9,9 +9,9 @@ import (
 func TestTerraformBasicExample(t *testing.T) {
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// website::tag::1::Set the path to the Terraform code that will be tested.
-		// The path to where our Terraform code is located
 		TerraformDir: "../examples/terraform-basic-example",
+
+		Logger: logger.Discard
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
