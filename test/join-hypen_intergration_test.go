bb0c067242fc1db6f56fc0414f951c095c437942
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-04-12 19:41:05 +0100

    Try Different Test

--- test/join-hypen_intergration_test.go
+++ test/join-hypen_intergration_test.go
@@ -43,3 +43,343 @@ func TestJoinHypenDeployment(t *testing.T) {
 	// Make sure the plan shows zero changes
 	assert.Contains(t, planResult, "No changes.")
 }
+
+func TestJoinHypenDeployment1(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment2(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment3(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment4(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment5(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment6(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment7(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeploymentFail(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment8(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
+
+func TestJoinHypenDeployment9(t *testing.T) {
+	t.Parallel()
+
+	_examplesDir := "../examples"
+	exampleDir := filepath.Join(_examplesDir, "join-hypen")
+
+	input_one := "one"
+	input_two := "two"
+	expected_output := "joined_name = \"one-two\""
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+
+		Vars: map[string]interface{}{
+			"example_input_one": input_one,
+			"example_input_two": input_two,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terratestOptions)
+
+	// Apply the infrastructure
+	applyResult := terraform.InitAndApply(t, terratestOptions)
+	assert.Contains(t, applyResult, expected_output)
+
+	// Run perpetual diff
+	planResult := terraform.Plan(t, terratestOptions)
+
+	// Make sure the plan shows zero changes
+	assert.Contains(t, planResult, "No changes.")
+}
