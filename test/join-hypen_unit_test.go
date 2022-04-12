4eb4bb81065655c49ea69bf48a656c371e77ea0b
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-04-12 19:44:31 +0100

    Try Different Test

--- test/join-hypen_unit_test.go
+++ test/join-hypen_unit_test.go
@@ -26,3 +26,173 @@ func TestJoinHypenValidity(t *testing.T) {
 
 	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
 }
+
+func TestJoinHypenValidity1(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity2(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity3(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity4(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity5(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity6(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidityFail(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "Some changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity7(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity8(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
+
+func TestJoinHypenValidity9(t *testing.T) {
+	t.Parallel()
+
+	_fixturesDir := "./fixtures"
+	exampleDir := filepath.Join(_fixturesDir, "join-hypen")
+
+	terratestOptions := &terraform.Options{
+		TerraformDir: exampleDir,
+		NoColor:      true,
+	}
+
+	t.Logf("Running in %s", exampleDir)
+	output := terraform.InitAndPlan(t, terratestOptions)
+
+	assert.Contains(t, output, "No changes. Your infrastructure matches the configuration.")
+}
